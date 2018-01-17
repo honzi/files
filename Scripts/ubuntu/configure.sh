e0440b4bc9df55edc85274fa28cbceb4bd7cd8a9
Jan Hořava <czechinthemail@gmail.com>
2018-01-17 02:12:03 -0800

    removed another package

--- Scripts/ubuntu/configure.sh
+++ Scripts/ubuntu/configure.sh
@@ -83,6 +83,7 @@ sudo apt-get remove libfreerdp-locale1.1
 sudo apt-get remove libfreerdp-primitives1.1
 sudo apt-get remove libfreerdp-utils1.1
 sudo apt-get remove libreoffice-common
+sudo apt-get remove librhythmbox-core9
 sudo apt-get remove mobile-broadband-provider-info
 sudo apt-get remove mythes-en-us
 sudo apt-get remove nautilus-sendto
