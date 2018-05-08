f50c5c1155455c48cd3ad3fe0cacf3df787407b2
Jan Hořava <czechinthemail@gmail.com>
2018-05-08 14:17:34 -0700

    added output usage

--- Scripts/ubuntu/16-04/autorun.sh
+++ Scripts/ubuntu/16-04/autorun.sh
@@ -3,7 +3,7 @@
 # No arguments.
 
 # Rescale.
-sh ~/.iterami/repositories/Scripts/ubuntu/xrandr-scale.sh 1600 900 1920 1080 1.2 1.2
+sh ~/.iterami/repositories/Scripts/ubuntu/xrandr-scale.sh eDP1 1600 900 1920 1080 1.2 1.2
 
 # Resetting.
 gsettings set org.gnome.desktop.peripherals.touchpad send-events disabled-on-external-mouse
