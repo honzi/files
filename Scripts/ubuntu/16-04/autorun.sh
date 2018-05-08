dce46251f7b552a2f3710901e2fdf21e57d1109b
Jan Hořava <czechinthemail@gmail.com>
2018-05-08 00:24:45 -0700

    added 16.04 autorun.sh

---
+++ Scripts/ubuntu/16-04/autorun.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# No arguments.
+
+# Rescale.
+sh ~/.iterami/repositories/Scripts/ubuntu/xrandr-scale.sh 1600 900 1920 1080 1.2 1.2
+
+# Resetting.
+gsettings set org.gnome.desktop.peripherals.touchpad send-events disabled-on-external-mouse
+gsettings set org.gnome.nautilus.preferences show-hidden-files true
