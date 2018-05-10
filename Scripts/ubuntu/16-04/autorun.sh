dc92184c93b950afd08f39123b9e9abb17c6047e
Jan Hořava <czechinthemail@gmail.com>
2018-05-09 22:18:15 -0700

    added gamma reset

--- Scripts/ubuntu/16-04/autorun.sh
+++ Scripts/ubuntu/16-04/autorun.sh
@@ -2,9 +2,10 @@
 
 # No arguments.
 
-# Rescale.
+# Adjust screen.
 sh ~/.iterami/repositories/Scripts/ubuntu/xrandr-scale.sh eDP1 1600 900 1920 1080 1.2 1.2
+xgamma -gamma 1
 
-# Resetting.
+# Reset settings.
 gsettings set org.gnome.desktop.peripherals.touchpad send-events disabled-on-external-mouse
 gsettings set org.gnome.nautilus.preferences show-hidden-files true
