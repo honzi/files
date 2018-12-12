e6827fcf8dff5753328f909ebe0f2693b5660508
Jan Hořava <czechinthemail@gmail.com>
2018-12-11 16:14:47 -0800

    improved sudo-fix.sh

--- Scripts/ubuntu/16-04/sudo-fix.sh
+++ Scripts/ubuntu/16-04/sudo-fix.sh
@@ -2,9 +2,6 @@
 
 # No arguments.
 
-# Empty the trash.
-sudo rm -rf ~/.local/share/Trash/*
-
 # Adjust screen.
 sh ~/.iterami/repositories/Scripts/ubuntu/xrandr-scale.sh eDP1 1600 900 1920 1080 1.2 1.2
 xgamma -gamma 1
@@ -13,6 +10,14 @@ xgamma -gamma 1
 gsettings set org.gnome.desktop.peripherals.touchpad send-events disabled-on-external-mouse
 gsettings set org.gnome.nautilus.preferences show-hidden-files true
 
-# Update and upgrade.
+# Empty the trash.
+sudo rm -rf ~/.local/share/Trash/*
+
+# Delete EVE logs.
+sudo rm -rf ~/.wine/drive_c/users/honzi/My\ Documents/EVE/logs/Gamelogs/*
+
+# Update everything.
 sudo apt-get update
 sudo apt-get upgrade
+sudo apt-get dist-upgrade
+sudo apt-get autoremove
