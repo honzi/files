d8e3a1d5ae4ddd73139cc81dd9966bb674aca849
Jan Hořava <czechinthemail@gmail.com>
2018-12-25 16:17:28 -0800

    added copy-index.sh

---
+++ Scripts/repositories/copy-index.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# No arguments.
+
+# Copy index.css and both index.htm for offline viewing.
+cp ~/.iterami/repositories/honzi.github.io/index.htm ~/.iterami/repositories/honzi.htm
+cp ~/.iterami/repositories/iterami.github.io/index.css ~/.iterami/repositories/
+cp ~/.iterami/repositories/iterami.github.io/index.htm ~/.iterami/repositories/
