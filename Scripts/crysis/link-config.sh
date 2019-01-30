a6f6d556dd353d6003df03481ac393f0481a6fd2
Jan Hořava <czechinthemail@gmail.com>
2019-01-30 13:01:48 -0800

    added crysis config link script

---
+++ Scripts/crysis/link-config.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# No arguments.
+
+# Create the symbolic link.
+ln -fs "$HOME/.iterami/repositories/files/Config/Crysis/diff_bauer.cfg" "$HOME/.wine/drive_c/Program Files (x86)/Electronic Arts/Crytek/Crysis SP Demo/Game/Config/diff_bauer.cfg"
