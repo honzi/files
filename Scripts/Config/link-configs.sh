c9728d0d5947efb5caa9aeabee56af8aa73b961f
Jan Hořava <czechinthemail@gmail.com>
2019-02-20 01:46:19 -0800

    absorbed crysis config link into generic config link script

--- Scripts/Config/link-configs.sh
+++ Scripts/Config/link-configs.sh
@@ -4,6 +4,7 @@
 
 # List of config file source paths.
 configs=(
+  "$HOME/.iterami/repositories/files/Config/Crysis/diff_bauer.cfg"
   "$HOME/.iterami/repositories/files/Config/GarrysMod/garrysmod/cfg/autoexec.cfg"
   "$HOME/.iterami/repositories/files/Config/Half-Life 2/hl2/cfg/autoexec.cfg"
   "$HOME/.iterami/repositories/files/Config/Half-Life 2/episodic/cfg/autoexec.cfg"
@@ -13,8 +14,9 @@ configs=(
   "$HOME/.iterami/repositories/files/Config/Portal 2/portal2/cfg/autoexec.cfg"
   "$HOME/.iterami/repositories/files/Config/Team Fortress 2/tf/cfg/autoexec.cfg"
 )
-# List of config file destination paths.
+# List of config file destination directory paths.
 destinations=(
+  "$HOME/.wine/drive_c/Program Files (x86)/Electronic Arts/Crytek/Crysis SP Demo/Game/Config/"
   "$HOME/.steam/steam/steamapps/common/GarrysMod/garrysmod/cfg/"
   "$HOME/.steam/steam/steamapps/common/Half-Life 2/hl2/cfg/"
   "$HOME/.steam/steam/steamapps/common/Half-Life 2/episodic/cfg/"
@@ -25,7 +27,7 @@ destinations=(
   "$HOME/.steam/steam/steamapps/common/Team Fortress 2/tf/cfg/"
 )
 
-for i in {0..7}
+for i in {0..8}
 do
     # The destination directory must already exist.
     if [[ -d ${destinations[i]} ]]
