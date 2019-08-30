eb6708f62a3a33514a035a2a5175fefa6e62e85f
Jan Hořava <czechinthemail@gmail.com>
2019-08-29 20:00:12 -0700

    started 18.04 fix script

---
+++ Scripts/Ubuntu/18-04/sudo-fix.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# No arguments.
+# Example usage: sh sudo-fix.sh
+
+# Adjust screen.
+sh ~/.iterami/repositories/Scripts/ubuntu/xrandr-scale.sh eDP1 1600 900 1920 1080 1.2 1.2
+xgamma -gamma 1
+
+# Reset settings.
+gsettings set org.gnome.desktop.peripherals.touchpad send-events disabled-on-external-mouse
+gsettings set org.gnome.nautilus.preferences show-hidden-files true
+
+# Cleanup various directories.
+sudo rm -rf ~/.local/share/Trash/*
+sudo rm -rf ~/.minecraft/crash-reports/*
+sudo rm -rf ~/.minecraft/logs/*
+sudo rm -rf ~/.wine/drive_c/users/honzi/My\ Documents/EVE/logs/Gamelogs/*
+
+# Update everything.
+sudo aptdcon -c --full-upgrade
