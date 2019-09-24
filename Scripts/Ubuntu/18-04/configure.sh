b0acc04da8820644c7dfb987f8386b475fee6e88
Jan Hořava <czechinthemail@gmail.com>
2019-09-24 14:37:23 -0700

    split configure.sh into install.sh and configure.sh

--- Scripts/Ubuntu/18-04/configure.sh
+++ Scripts/Ubuntu/18-04/configure.sh
@@ -3,40 +3,6 @@
 # No arguments.
 # Example usage: sh configure.sh
 
-# UPDATE
-sudo apt-get update
-sudo apt-get dist-upgrade
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get autoremove
-
-
-# INSTALL
-mkdir -p ~/.iterami/
-mkdir -p ~/.iterami/repositories/
-mkdir -p ~/.iterami/storage/
-
-cd ~/.iterami/repositories/
-git clone --depth 1 https://github.com/honzi/files.git
-cd files/Scripts
-sh fetch-iterami-honzi-repos.sh ~/.iterami/repositories/
-
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get autoremove
-
-
-# UNINSTALL
-sudo apt-get remove bluez
-sudo apt-get remove gnome-bluetooth
-sudo apt-get remove indicator-bluetooth
-sudo apt-get remove pulseaudio-module-bluetooth
-
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get autoremove
-
-
 # CONFIGURE
 git config --global user.name "Jan Hořava"
 git config --global user.email czechinthemail@gmail.com
