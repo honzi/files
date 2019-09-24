6bbcfcbbecea347cca08c405c2481c967ceefeee
Jan Hořava <czechinthemail@gmail.com>
2019-09-24 13:24:17 -0700

    added black background gsettings

--- Scripts/Ubuntu/18-04/configure.sh
+++ Scripts/Ubuntu/18-04/configure.sh
@@ -40,3 +40,7 @@ sudo apt-get autoremove
 # CONFIGURE
 git config --global user.name "Jan Hořava"
 git config --global user.email czechinthemail@gmail.com
+gsettings set org.gnome.desktop.background color-shading-type "solid"
+gsettings set org.gnome.desktop.background picture-options "none"
+gsettings set org.gnome.desktop.background primary-color "#000000"
+gsettings set org.gnome.desktop.background secondary-color "#000000"
