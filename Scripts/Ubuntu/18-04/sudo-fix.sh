d473af34056754ff0168f647a46fd9e43351b232
Jan Hořava <czechinthemail@gmail.com>
2019-10-14 15:12:05 -0700

    added indicator-messages-service killall to sudo fix, combined screen adjustment into settings reset

--- Scripts/Ubuntu/18-04/sudo-fix.sh
+++ Scripts/Ubuntu/18-04/sudo-fix.sh
@@ -3,11 +3,9 @@
 # No arguments.
 # Example usage: sh sudo-fix.sh
 
-# Adjust screen.
+# Reset various settings.
 sh ~/.iterami/repositories/Scripts/ubuntu/xrandr-scale.sh eDP1 1600 900 1920 1080 1.2 1.2
 xgamma -gamma 1
-
-# Reset settings.
 xinput disable 'ETPS/2 Elantech Touchpad'
 
 # Cleanup various directories.
@@ -19,6 +17,9 @@ sudo rm -rf ~/snap/warzone2100/current/.local/share/warzone2100-3.3.0/logs/*
 sudo rm -rf ~/.steam/steam/steamapps/common/Portal\ 2/portal2/maps/workshop/*
 sudo rm -rf ~/.wine/drive_c/users/honzi/My\ Documents/EVE/capture/Screenshots/*
 sudo rm -rf ~/.wine/drive_c/users/honzi/My\ Documents/EVE/logs/Gamelogs/*
+
+# Stop various processes.
+killall indicator-messages-service
 
 # Update everything.
 sudo apt-get update
