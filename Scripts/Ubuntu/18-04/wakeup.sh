d84241bd2aa13c0507dd1b36868f2a433718fc0e
Jan Hořava <czechinthemail@gmail.com>
2020-05-16 19:00:36 -0700

    added minecraft launcher log clear

--- Scripts/Ubuntu/18-04/wakeup.sh
+++ Scripts/Ubuntu/18-04/wakeup.sh
@@ -10,6 +10,7 @@ sudo -K
 # Cleanup various directories.
 > ~/.bash_history
 > ~/.python_history
+> ~/.minecraft/launcher_log.txt
 sudo rm -rf ~/.cache/thumbnails/*
 sudo rm -rf ~/.local/share/Trash/*
 sudo rm -rf ~/.minecraft/crash-reports/*
