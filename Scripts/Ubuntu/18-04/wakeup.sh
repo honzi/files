e49420dba5e3fbdee519c107071035666683071b
Jan Hořava <czechinthemail@gmail.com>
2020-11-23 18:39:52 -0800

    added wakeup

---
+++ Scripts/Ubuntu/18-04/wakeup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -eu
+
+# No args.
+# Example usage: sh wakeup.sh
+
+# UPDATE
+sh ~/.iterami/repositories/files/Scripts/Ubuntu/18-04/update.sh
+
+
+# CONFIGURE
+sh ~/.iterami/repositories/files/Scripts/Ubuntu/18-04/configure.sh
