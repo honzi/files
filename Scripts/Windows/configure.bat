d36db84d101a77fd6cc718730b94d33d87d9b206
Jan Hořava <czechinthemail@gmail.com>
2021-08-08 15:57:54 -0700

    added example usage

--- Scripts/Windows/configure.bat
+++ Scripts/Windows/configure.bat
@@ -1,6 +1,7 @@
 @ECHO OFF
 
 :: No args.
+:: Example usage: configure.bat
 
 :: Registry changes.
 REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "ShowSecondsInSystemClock" /t "REG_DWORD" /d "1" /f
