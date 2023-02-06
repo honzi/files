e4f4137bccb989667f73fec52992b2a575bd0776
Jan Hořava <czechinthemail@gmail.com>
2023-02-05 16:15:46 -0800

    added strings.sh to search all repos for strings to change

---
+++ Scripts/Repos/strings.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+set -eux
+
+# Required args:
+#   $1: Relative path to the folder in which
+#         the iterami repos are stored.
+#
+# Example usage: sh strings.sh repos/
+
+# Check if at least 1 arg was passed.
+if [ $# -lt 1 ]
+then
+    echo 'Missing arg: path'
+    exit 1
+fi
+
+cd $1
+
+strings='
+argument
+combination
+configuration
+conversation
+corporation
+difference
+documentation
+HTML
+repository
+statistics
+though
+through
+'
+
+for string in $strings
+do
+    grep -lr --exclude-dir=".git" --exclude-dir="OldCode" --exclude="LICENSE.md" "$string"
+    echo
+done
