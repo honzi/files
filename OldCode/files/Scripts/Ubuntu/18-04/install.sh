ee37115b8aba932a6f3ec2e018dc8809d3f27836
Jan Hořava <czechinthemail@gmail.com>
2023-04-29 12:13:55 -0700

    moved 18.04 scripts to old code

---
+++ OldCode/files/Scripts/Ubuntu/18-04/install.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+set -eux
+
+# No args.
+# Example usage: sh install.sh
+
+# UPDATE
+sh ~/.iterami/repos/files/Scripts/Ubuntu/update.sh
+
+
+# INSTALL
+sudo apt-get install dconf-editor
+sudo apt-get install gdebi-core
+sudo apt-get install git
+sudo apt-get install lm-sensors
+sudo apt-get install ubuntu-restricted-extras
+
+mkdir -p ~/.iterami/
+mkdir -p ~/.iterami/repos/
+mkdir -p ~/.iterami/storage/
+
+cd ~/.iterami/repos/
+git clone https://github.com/honzi/files.git --depth 1
+cd files/Scripts
+sh fetch-iterami-honzi-repos.sh ~/.iterami/repos/
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get autoremove
+
+
+# UNINSTALL
+sudo apt-get remove bluez
+sudo apt-get remove gnome-bluetooth
+sudo apt-get remove indicator-bluetooth
+sudo apt-get remove pulseaudio-module-bluetooth
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get autoremove
+
+
+# CONFIGURE
+sh ~/.iterami/repos/files/Scripts/Ubuntu/configure.sh
---
+++ OldCode/files/Scripts/Ubuntu/18-04/wakeup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -eux
+
+# No args.
+# Example usage: sh wakeup.sh
+
+# CONFIGURE
+sh ~/.iterami/repos/files/Scripts/Ubuntu/configure.sh
+
+
+# UPDATE
+sh ~/.iterami/repos/files/Scripts/Ubuntu/update.sh
