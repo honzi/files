977d5bc25e3d4f9ccffe0448b13871fe4ae92a05
Jan Hořava <czechinthemail@gmail.com>
2023-04-29 14:47:16 -0700

    added some config back in

--- Scripts/Ubuntu/install.sh
+++ Scripts/Ubuntu/install.sh
@@ -8,6 +8,7 @@ set -eux
 sh ~/.iterami/repos/files/Scripts/Ubuntu/update.sh
 
 # INSTALL
+sudo apt-get install dconf-editor
 sudo apt-get install git
 
 mkdir -p ~/.iterami/
@@ -24,6 +25,10 @@ sudo apt-get upgrade
 sudo apt-get autoremove
 
 # UNINSTALL
+sudo apt-get remove bluez
+sudo apt-get remove gnome-bluetooth
+sudo apt-get remove indicator-bluetooth
+
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get autoremove
