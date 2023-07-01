c3df8d9fa27453f2c65b2a823f6d2d13f69654a2
Jan Hořava <czechinthemail@gmail.com>
2023-06-30 19:15:11 -0700

    added transferred repos

--- Scripts/Repos/honzi-repos-list.sh
+++ Scripts/Repos/honzi-repos-list.sh
@@ -7,6 +7,12 @@ set -eux
 # An array of all honzi repos.
 honzi_repos='
 biggestisk
+EVE-Overviews.yaml
 files
+GMod-Addons.lua
 honzi.github.io
+MC-DataPacks
+SC-AI.cpp
+WoW-Addons.lua
+WZ2100-AI.js
 '
