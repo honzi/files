a7f54a4209bc9143285a1cc3d42537f57f0fab88
Jan Hořava <czechinthemail@gmail.com>
2023-07-08 12:42:23 -0700

    simplified eve cleanup

--- Scripts/Ubuntu/cleanup.sh
+++ Scripts/Ubuntu/cleanup.sh
@@ -15,6 +15,5 @@ sudo rm -rf ~/.minecraft/logs/*
 sudo rm -rf ~/snap/warzone2100/current/.local/share/*/logs/*
 sudo rm -rf ~/.steam/steam/steamapps/common/Portal\ 2/portal2/maps/workshop/*
 sudo rm -rf ~/.wine/drive_c/Program\ Files\ \(x86\)/Electronic\ Arts/Crytek/Crysis\ SP\ Demo/LogBackups/*
-sudo rm -rf ~/.wine/drive_c/users/honzi/My\ Documents/EVE/capture/Portraits/*
-sudo rm -rf ~/.wine/drive_c/users/honzi/My\ Documents/EVE/capture/Screenshots/*
-sudo rm -rf ~/.wine/drive_c/users/honzi/My\ Documents/EVE/logs/Gamelogs/*
+sudo rm -rf ~/.wine/drive_c/users/honzi/My\ Documents/EVE/capture/*/*
+sudo rm -rf ~/.wine/drive_c/users/honzi/My\ Documents/EVE/logs/*/*
