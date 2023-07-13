b7c67c041f314450d62f8af2f3c20af45806bee1
Jan Hořava <czechinthemail@gmail.com>
2023-07-12 20:52:41 -0700

    cleared poe log

--- Scripts/Windows/cleanup.bat
+++ Scripts/Windows/cleanup.bat
@@ -4,6 +4,7 @@
 :: Example usage: cleanup.bat
 
 :: Cleanup various folders.
+break>"C:\Program Files (x86)\Grinding Gear Games\Path of Exile\logs\Client.txt"
 del /q /s "C:\Users\janho\Documents\EVE\capture\*"
 del /q /s "C:\Users\janho\Documents\EVE\logs\*"
 del /q /s "C:\Users\janho\Music\*"
