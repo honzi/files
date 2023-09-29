7e41cb8a9a6d6049645c28cf474c7c51135d22ca
Jan Hořava <czechinthemail@gmail.com>
2023-09-29 13:09:06 -0700

    added offline scripts for ubuntu and windows

---
+++ Scripts/Ubuntu/offline.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -eux
+
+# No args.
+# Example usage: sh offline.sh
+
+# CONFIGURE
+sh ~/iterami/repos/files/Scripts/Ubuntu/configure.sh
+
+# CLEANUP
+sh ~/iterami/repos/files/Scripts/Ubuntu/cleanup.sh
---
+++ Scripts/Windows/offline.bat
@@ -0,0 +1,12 @@
+:: No args.
+:: Example usage: offline.bat
+
+cd "C:\Users\janho\Documents\iterami\repositories\files\Scripts\Windows"
+
+:: CONFIGURE
+call configure
+
+:: CLEANUP
+call cleanup
+
+PAUSE
