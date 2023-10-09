85c940a842270fcf23de861f135fab1f61af2b06
Jan Hořava <czechinthemail@gmail.com>
2023-10-08 23:31:48 -0700

    removed upgrade and 2nd update, added autoclean

--- Scripts/Ubuntu/update.sh
+++ Scripts/Ubuntu/update.sh
@@ -7,7 +7,6 @@ set -eux
 # UPDATE
 sudo apt-get -y update
 sudo apt-get -y dist-upgrade
-sudo apt-get -y update
-sudo apt-get -y upgrade
+sudo apt-get -y autoclean
 sudo apt-get -y autoremove
 sudo snap refresh
